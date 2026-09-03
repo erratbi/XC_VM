94a4e84f21e257aca978bca030e5e07ffddb5761
labssin <labssinmaroc@gmail.com>
2026-09-03 20:33:24 +0100

    fix(docker): use Authenticator::hashPassword for auto-seeded administrator account

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -199,7 +199,7 @@ if (\$db) {
         \$user = '${ADMIN_USER}';
         \$pass = '${ADMIN_PASS}';
         \$email = '${ADMIN_EMAIL}';
-        \$hash = crypt(\$pass, '\\\$6\\\$rounds=20000\\\$' . md5(random_bytes(16)) . '\\\$');
+        \$hash = \XcVm\Core\Auth\Authenticator::hashPassword(\$pass);
         \$db->query('INSERT INTO \`users\` (\`username\`, \`password\`, \`email\`, \`member_group_id\`, \`date_registered\`, \`last_login\`, \`ip\`, \`status\`) VALUES (?, ?, ?, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), \"127.0.0.1\", 1)', \$user, \$hash, \$email);
         \$db->query('UPDATE \`servers\` SET \`server_ip\` = \"127.0.0.1\" WHERE \`is_main\` = 1 LIMIT 1');
         \$db->query('UPDATE \`settings\` SET \`ffmpeg_cpu\` = \"8.0\", \`ffmpeg_gpu\` = \"8.0\" WHERE \`id\` = 1');
